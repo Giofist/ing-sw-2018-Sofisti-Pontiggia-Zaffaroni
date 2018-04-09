65005c202058cf5d2bd9c16bf9e30514560649c9
francescopont <37176513+francescopont@users.noreply.github.com>
2018-04-09 19:54:37 +0200

    un altro commit di prova, love you all

---
+++ prova_temp/src/main/java/ing-sw-2018-Sofisti-Pontiggia-Zaffaroni/App.java
@@ -0,0 +1,13 @@
+package ing-sw-2018-Sofisti-Pontiggia-Zaffaroni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ prova_temp/src/test/java/ing-sw-2018-Sofisti-Pontiggia-Zaffaroni/AppTest.java
@@ -0,0 +1,20 @@
+package ing-sw-2018-Sofisti-Pontiggia-Zaffaroni;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
