0615cf8f9bc643eb42e92305f5cdaa85209f8a3c
francescopont <37176513+francescopont@users.noreply.github.com>
2018-04-26 15:19:02 +0200

    new cose

---
+++ Sagrada/src/main/java/it/polimi/ingsw/model/Exceptions/PrivateGoalCardException.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.model.Exceptions;
+
+public class PrivateGoalCardException extends Exception {
+    private static final String msg = "Something went wrong with private goal Card: zorry mate!";
+    public PrivateGoalCardException() {
+        super(msg);
+    }
+    public PrivateGoalCardException(String msg) {
+        super(msg);
+    }
+}
---
+++ Sagrada/src/main/java/it/polimi/ingsw/model/PrivateGoalCards/PrivateGoalCardDeck.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.model.PrivateGoalCards;
+
+
+import it.polimi.ingsw.model.Exceptions.PrivateGoalCardException;
+import it.polimi.ingsw.model.GoalCard;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+//command pattern just like schemecarddeck
+public class PrivateGoalCardDeck {
+    private ArrayList<Integer> cards;
+    //constructor
+    public PrivateGoalCardDeck() {
+        this.cards = new ArrayList<>();
+        for(int i=1; i<6;i++){
+            this.cards.add(i);
+        }
+
+        Collections.shuffle(cards);
+
+    }
+    public GoalCard getCard() throws PrivateGoalCardException{
+        int CardID = this.getRandomID();
+        switch (CardID) {
+            case 1: return new SfumatureRosse();
+            case 2: return new SfumatureGialle();
+            case 3: return new SfumatureVerdi();
+            case 4: return new SfumatureBlu();
+            case 5: return new SfumatureViola();
+            default: throw new PrivateGoalCardException();  //add an exception here;
+        }
+    }
+
+
+    public int getRandomID(){
+        int value = this.cards.get(0);
+        this.cards.remove(0);
+        return value;
+    }
+
+}
