4f1103de395daad4d9ddbe370c317afe20205781
Diego Zaffaroni <diego1.zaffaroni@mail.polimi.it>
2018-06-27 01:31:03 +0200

    Miglioramento copertura test e nuovi test

---
+++ Sagrada/src/test/java/it/polimi/ingsw/model/RoundTest.java
@@ -0,0 +1,78 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.PlayerPackage.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+// CANNOT TEST WAITING METHOD
+
+public class RoundTest {
+
+    private Round round;
+    private Player mockPlayer1;
+    private Player mockPlayer2;
+    private Match mockMatch;
+    private LinkedList<Player> players;
+    private Gametable mockGametable;
+
+    @Before
+    public void before() {
+        // Mock classes
+        mockPlayer1 = mock(Player.class);
+        mockPlayer2 = mock(Player.class);
+        mockMatch = mock(Match.class);
+        mockGametable = mock(Gametable.class);
+
+        players = new LinkedList<>();
+        players.add(mockPlayer1);
+        players.add(mockPlayer2);
+
+
+        // Mock behaviour
+        when(mockMatch.getGametable()).thenReturn(mockGametable);
+        when(mockMatch.getNumberOfPlayers()).thenReturn(2);
+        when(mockPlayer1.getMatch()).thenReturn(mockMatch);
+        when(mockPlayer2.getMatch()).thenReturn(mockMatch);
+        doNothing().when(mockGametable).setupRound();
+
+    }
+
+
+    @Test
+    public void runTest() throws InterruptedException {
+        Thread testThread = new Thread(new RoundTestThread());
+        testThread.start();
+
+        Thread.sleep(2000);
+        testThread.interrupt();
+    }
+
+    // Getters
+    @Test
+    public void getMatchTest() {
+        round = new Round(1, players, mockMatch);
+        assertEquals(mockMatch, round.getMatch());
+    }
+
+    @Test
+    public void getNumRoundTest() {
+        round = new Round(1, players, mockMatch);
+        assertEquals(1, round.getNum_round());
+    }
+
+    @Test
+    public void getPlayersTest() {
+        round = new Round(1, players, mockMatch);
+        assertEquals(players, round.getPlayers());
+    }
+
+}
